bdfc9482c2e4459e277d4e3a46d2a96c1c8f2ea4
Jonathan Engelsma <jengelsma@gmail.com>
2022-06-03 12:28:47 -0400

    Part H

--- screens/YTViewerScreen.js
+++ screens/YTViewerScreen.js
@@ -1,15 +1,28 @@
+import { StyleSheet, View } from "react-native";
+
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { WebView } from "react-native-webview";
 
 const YTViewerScreen = ({ route }) => {
   console.log("YTViewerScreen: ", route.params);
   return (
-    <View>
-      <Text> YTViewerScreen </Text>
+    <View style={styles.screen}>
+      <WebView
+        javaScriptEnabled
+        domStorageEnabled
+        source={{
+          uri: `https://www.youtube.com/embed/${route.params.id.videoId}`,
+        }}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+});
+
 
 export default YTViewerScreen;
